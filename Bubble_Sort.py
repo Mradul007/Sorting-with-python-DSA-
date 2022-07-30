38ddae58de0472d3e8fcc4e22346bed88f63ef1a
Mradul007 <104089415+Mradul007@users.noreply.github.com>
2022-07-31 00:08:13 +0530

    Update Bubble_Sort.py

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,8 +1,19 @@
+# Input unsorted array
 li = [int(x) for x in input().split(' ')]
+
+# Traverse through all array elements
 for i in range(len(li)):
+    
+    # Last i elements are already in place
     for j in range(len(li)-1-i):
+        
+        # traverse the array from 0 to n-i-1
+        # Swap if the element found is greater
+        # than the next element
         if li[j] > li[j+1]:
             temp = li[j]
             li[j] = li[j+1]
             li[j+1] = temp
+            
+# Output sorted array
 print(li)
